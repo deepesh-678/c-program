79f038aa3f41b1a1a187d57621fce9656f486f8d
deepesh-678 <113338558+deepesh-678@users.noreply.github.com>
2022-09-21 10:17:08 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+   int n;
+   printf("Enter  the number to print table : ");
+   scanf("%d",&n);
+   for(j=1;j<=10;j++)
+   {
+     printf("%d X %d = %d \n", n);
+   }
+} 
