227e6aaa6b24170b4a0e56cc3f0ef4bdd8fd5c5b
deepesh-678 <113338558+deepesh-678@users.noreply.github.com>
2022-09-21 10:27:04 +0530

    Add files via upload

---
+++ negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()  {
+    int i, n, count = 0, arr[] = {0, 160, 180, 270, 160, 230, 210, 190, 0};
+    n = sizeof(arr)/sizeof(arr[0]);
+
+    for(i=0; i<n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            count++;
+        }
+    }
+
+    printf("Total number of negative numbers in array: %d", count);
+}
+
+
+
